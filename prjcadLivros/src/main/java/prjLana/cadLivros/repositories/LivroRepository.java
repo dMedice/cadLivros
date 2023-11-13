8e67166188c9d9102472142bdbb3e02d0a154355
dMedice <133895157+dMedice@users.noreply.github.com>
2023-11-13 14:17:10 -0300

    Delete prjcadLivros directory

--- prjcadLivros/src/main/java/prjLana/cadLivros/repositories/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package prjLana.cadLivros.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import prjLana.cadLivros.entities.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-
-}
